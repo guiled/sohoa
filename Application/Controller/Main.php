92b2fbe783ddb18c6bf3b3d9aa4942d654a3e77d
Guiled <guislain.duthieuw@gmail.com>
2014-10-26 11:27:32 +0100

    PHP5.4 minimum requirement and remove namespace indentation

--- Application/Controller/Main.php
+++ Application/Controller/Main.php
@@ -1,22 +1,18 @@
 <?php
 
-namespace Application\Controller {
+namespace Application\Controller;
 
-    use Sohoa\Framework\Kit;
+use Sohoa\Framework\Kit;
 
-    class Main extends Kit
-    {
+class Main extends Kit {
 
-        public function indexAction()
-        {
-            echo 'Bouya xD';
-
-        }
-
-        public function sampleAction() { // Routed by /Main/Sample/ with generic route
-
-            $this->data->sample  = ': Gordon foobar';
-            $this->greut->render(); // Go to hoa://Application/View/Main/Foo.tpl.php
-        }
+    public function indexAction() {
+        echo 'Bouya xD';
     }
+
+    public function sampleAction() { // Routed by /Main/Sample/ with generic route
+        $this->data->sample = ': Gordon foobar';
+        $this->greut->render(); // Go to hoa://Application/View/Main/Foo.tpl.php
+    }
+
 }
---
+++ Binaries/sohoa.bat
@@ -0,0 +1,3 @@
+@ECHO OFF
+SET BIN_TARGET=%~dp0/../vendor/sohoa/framework/Sources/Sohoa/Bin/sohoa
+php "%BIN_TARGET%" %*
